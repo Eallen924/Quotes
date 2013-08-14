2342410ee463aff9d5155620d8281a6bc5ee7865
Eric Allen <eric.justin.allen@gmail.com>
2013-08-13 21:50:24 -0500

    f

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -17,9 +17,26 @@ get '/authors' do
   erb :list_authors
 end
 
-post '/quote/get' do
+get '/quote' do
+
+  puts "quote -- ive been called"
+  puts "*" * 80
   @quote = Quote.all.sample
-  p @quote
-  erb :_quote, layout: false
+  puts "*" * 80
+  p @quote.body
+  puts "*" * 80
+  erb :_quote, layout: !request.xhr?
 end
 
+get '/test' do
+  p "hit the route"
+  "this is a result" if request.xhr?
+end
+
+
+# if request.xhr?
+#   content_type :json
+#   {body: @quote.body, author: @quote.author.name}.to_json
+# else
+#   erb :_quote
+# end
--- public/js/application.js
+++ public/js/application.js
@@ -1,25 +1,101 @@
-$(document).ready(function() { //start after HTML, images have loaded
-  var InfiniteRotator = {
-    init: function() {
-      //interval between items (in milliseconds)
-      var itemInterval = 10000;
-      //set current item
-      var getCurrentItem = function() {
-        $.post('/quote/get', function(newQuote) {
-          // console.log(newQuote)
-          $('#rotating-item-wrapper').append(newQuote);
-        })
-      };
+function displayQuote(startTime) {
+   // log time difference between quote calls to verify the correct interval length
+   console.log("Waited " + (new Date() - startTime));
 
-      //show first item
+   // ajax request to get new quote
+  $.get('/quote', function(returnData){
+    var $rotator = $('#rotating-item-wrapper');
+    $rotator.fadeOut(1500, function() {
+      $rotator.html(returnData).fadeIn(1500);
+    });
 
-      //loop through the items
-      var infiniteLoop = setInterval(function(){
-        $('#rotating-item-wrapper').children(".rotating-item").remove();
-        $('#rotating-item-wrapper').html(getCurrentItem());
-      }, itemInterval);
-          debugger
-    }
-  };
-  InfiniteRotator.init();
+    var quote = $(returnData).find('.quote_body').text();
+    var quoteChars = quote.length;
+    var quoteTime = quoteChars * 120
+
+    console.log("This quote has " + quoteChars + " characters and will be displayed for " + quoteTime/1000 + "seconds");
+
+    var startTime = new Date();
+    // recursively display quotes
+    setTimeout(function() { displayQuote(startTime) }, quoteTime);
+  });
+};
+
+$(document).ready(function(){
+  // initialize first timeout with a 10 second interval
+  var quoteTime = 10000;
+  // initialize a start time to verify how long it takes between quotes
+  var startTime = new Date();
+  setTimeout(function() { displayQuote(startTime) }, quoteTime);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function displayQuote(quoteObject) {
+//   console.log(quoteObject);
+//     var quoteHtml = '<div class="rotating-item"><p class="quote_body">"'+ quoteObject.body +'"</p><p class="quote_author"> - ' + quoteObject.author +' - </p></div>'
+//     var $rotator = $('#rotating-item-wrapper');
+//     $rotator.fadeOut(1500, function() {
+//     $rotator.html(quoteHtml).fadeIn(1500);
+//     });
+
+//   $.get('/quote', function(returnData){
+//     console.log(returnData.body);
+//     console.log(returnData.author);
+
+//     var quoteChars = returnData.body.length;
+//     console.log(quoteChars);
+//     var quoteTime = quoteChars * 120
+//     console.log(quoteTime);
+//     setTimeout(displayQuote(returnData), quoteTime);
+//   });
+// };
+
+// $(document).ready(function(){
+//   $.get('/quote', function(returnData){
+//     console.log(returnData.body);
+//     console.log(returnData.author);
+//     var quoteChars = returnData.body.length;
+//     console.log(quoteChars);
+//     var quoteTime = quoteChars * 120
+//     console.log(quoteTime);
+
+//     setTimeout(displayQuote(returnData), quoteTime);
+//   });
+// });
